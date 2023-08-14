648272f78ac92d0a569d312ae2542d1842cedfb1
Walter Rodrigues <metal.walter@gmail.com>
2023-08-14 18:05:57 -0300

    Create Service and DbContext classes

---
+++ PIX/Data/MVCPixDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PIX.Models.Domain;
+
+namespace PIX.Data
+{
+    public class MVCPixDbContext : DbContext
+    {
+        public MVCPixDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Service> Services { get; set; }
+    }
+}
