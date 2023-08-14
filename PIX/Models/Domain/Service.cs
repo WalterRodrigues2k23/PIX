bb66809a13ea8f5b060783ff53b629fa6db3f8a2
Walter Rodrigues <metal.walter@gmail.com>
2023-08-14 18:04:30 -0300

    Create Class Service and DbContext

---
+++ PIX/Models/Domain/Service.cs
@@ -0,0 +1,12 @@
+﻿namespace PIX.Models.Domain
+{
+    public class Service
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int CPF { get; set; }
+        public DateTime DateOfTransaction { get; set; }
+        public long Amount { get; set; }
+    }
+}
