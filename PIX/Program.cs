f9bd2a7fb5439d58321df8beeacafe879c2452dc
Walter Rodrigues <metal.walter@gmail.com>
2023-08-15 10:40:09 -0300

    Conexão com banco de Dados SQL Feita

--- PIX/Program.cs
+++ PIX/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PIX.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCPixDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MVCPixConnectionString")));
 
 var app = builder.Build();
 
