3e7780b4fc72c49de515e8d7284abfd7f39146e6
Walter Rodrigues <metal.walter@gmail.com>
2023-08-16 10:50:44 -0300

    Controle das transações criado

--- PIX/Program.cs
+++ PIX/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<MVCPixDbContext>(options => 
+
+builder.Services.AddDbContext<MVCPixDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MVCPixConnectionString")));
 
 var app = builder.Build();
